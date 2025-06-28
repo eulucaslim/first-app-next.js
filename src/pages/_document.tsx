99b0f62266b29ef04ea20af6e2a810b98b4d03f3
eulucaslim <lucaslimacnt14@gmail.com>
2025-06-28 17:04:05 -0400

    Add the dinamic title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
-                    <title>ig.news</title>
                 </Head>
                 <body>
                     <Main />
