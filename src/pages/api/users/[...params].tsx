26560a54c357f3f27043ebf344fad77dc91ca551
eulucaslim <lucaslimacnt14@gmail.com>
2025-07-05 14:45:54 -0400

    Using [...params] in routes

--- src/pages/api/users/[...params].tsx
+++ src/pages/api/users/[...params].tsx
@@ -1,8 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-// Serverless
-
 export default (request: NextApiRequest, response: NextApiResponse) => {
+    const { params } = request.query
+    console.log(params)
+
+
     const users = [
         { id: 1, name: 'Diego' },
         { id: 2, name: 'Dani' },
