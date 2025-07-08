43f4702bbcefbf5d5418b41e58349257b1701dc8
eulucaslim <lucaslimacnt14@gmail.com>
2025-07-08 18:25:07 -0400

    Add user email to database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { supabase } from '../../../services/supabase';
+import { PostgrestError } from "@supabase/supabase-js";
+
 
 export default NextAuth({
     providers: [
@@ -10,7 +13,32 @@ export default NextAuth({
             params: {
                 scope: "read:user", 
             },
-        }   
+        }
       }),
-    ]
+    ],
+    callbacks: {
+      async signIn({ user, account, profile}) {
+        const { email } = user
+        try {
+          const { data, error } = await supabase
+            .from('users')
+            .select()
+          
+          const userEmail = data?.find(user => user.email == email)
+          
+          if (!userEmail) {
+            await supabase.from('users').insert({email: email})
+            return true
+          }
+          else {
+            return true
+          }
+          
+        } catch (error){
+          console.log(error)
+          return false
+        }
+          
+      }
+    }
 })
