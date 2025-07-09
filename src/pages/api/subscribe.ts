2bdc0eece7f855573483baab70ebbacdba586896
eulucaslim <lucaslimacnt14@gmail.com>
2025-07-08 20:22:36 -0400

    Add a validation to stripe save in database

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,38 @@
 import { stripe } from "@/services/stripe";
 import { getSession } from "next-auth/react";
 import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "@/services/supabase";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const session =  await getSession({ req })
+        
+        const { data: user, error: userFoundError } = await supabase
+        .from('users')
+        .select('*')
+        .eq('email', session?.user?.email)
+        .single()
 
-        const stripeCostumer = await stripe.customers.create({
-            email: session?.user?.email!
-            // metadata
-        });
+        userFoundError ? console.log(userFoundError) : null
+
+        let costumerId = user.stripe_customer_id
+
+        if (!costumerId) {
+            const stripeCostumer = await stripe.customers.create({
+                email: session?.user?.email!
+            });
+            
+            const { error } = await supabase
+            .from('users')
+            .update({stripe_customer_id: stripeCostumer.id})
+            .match({id: user.id, email: user.email})
+            
+            error ? console.log(error) : null
+            costumerId = stripeCostumer.id
+        }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCostumer.id,
+            customer: costumerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
